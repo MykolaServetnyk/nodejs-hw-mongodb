84631fb3ba30ea457c056c8d8a6e3745c3b6ec91
MykolaServetnyk <mikokobass@gmail.com>
2024-06-20 00:01:07 +0300

    first try

--- src/server.js
+++ src/server.js
@@ -1,9 +1,72 @@
+// src/server.js
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
+import pino from 'pino-http';
 
-const app = express();
-app.use(cors());
-const PORT = 3000;
-const setupServer = () => {
+import { env } from './utils/env.js';
+import { getContacts, getContactById } from './services/contactServices.js';
 
-}
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+
+    const logger = pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+    });
+    app.use(logger);
+
+    app.get('/api/contacts', async (req, res) => {
+        try {
+            const data = await getContacts();
+            res.json({
+                status: 200,
+                data,
+                message: 'Successfully found contacts!',
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+    });
+
+    app.get('/api/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+        try {
+            const data = await getContactById(contactId);
+            if (!data) {
+                return res.status(404).json({
+                    message: 'Not found!',
+                });
+            }
+            res.json({
+                status: 200,
+                data,
+                message: `Successfully found contact with id ${contactId}!`,
+            });
+        } catch (error) {
+            if (error.message.includes('Cast to ObjectId failed')) {
+                error.status = 404;
+            }
+            const { status = 500 } = error;
+            res.status(status).json({
+                message: error.message,
+            });
+        }
+    });
+
+    app.use((req, res, next) => {
+        res.status(404).json({
+            message: 'Not found',
+        });
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
